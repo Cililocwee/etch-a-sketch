d85cc32eb0a098703e414c7fdaebd0f5a3a114fd
Corrie Stroup <Corrie.Stroup@gmail.com>
2022-08-30 13:48:56 +0700

    I'm lost
    Currently edited some things, but totally lost the thread. Starting a new branch, if I come back to this branch, I need to revert back a few commits to clear things up

--- scripts.js
+++ scripts.js
@@ -1,15 +1,19 @@
+
+
 function gridGenerator(number){  
     let i = 0;
     let n = number;
-  
+
+    // creates an N amount of rows, an N amount of times
     container.innerHTML = 
-      `<div class="row">${'<div class="cell">X</div>'.repeat(n)}</div>`
-      .repeat(n).replace(/X/g,_=> (i++).toString(n) )
-  }
-  
+      `<div class="row">${'<div class="cell" id="x">x</div>'.repeat(n)}</div>`
+      .repeat(n).replace(/x/g,_=> (i++).toString(n));  //replaces x with i++
+} 
+
 var slider = document.getElementById("myRange");
 var output = document.getElementById("slidervalue");
 output.innerHTML = slider.value; //display the default slider value
+
 
 // update the current slider value (each time you drag the slider handle)
 slider.oninput = function() {
@@ -17,4 +21,11 @@ slider.oninput = function() {
     gridGenerator(this.value);
 }
 
-gridGenerator(5);
+var color = ["black","blue", "pink","yellow"];
+
+function changeColor(){
+    document.querySelector('.cell').style.background
+    = color[Math.floor(Math.random() * color.length)];
+}
+
+gridGenerator(11);
